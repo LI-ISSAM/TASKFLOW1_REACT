a5f3b35892ca331699fb7bd934ba63bcecd24d46
LITIMI ISSAM <litimi.issam20@gmail.com>
2026-04-15 11:02:33 +0200

    sauvegarde avant merge

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,28 +1,30 @@
-import styles from './MainContent.module.css'; 
-
+import styles from './MainContent.module.css';
 
 interface Column {
-    id: string;
-    title : string;
-    tasks : string[]
+  id: string;
+  title: string;
+  tasks: string[];
 }
+
 interface MainContentProps {
-    columns : Column [];
+  columns: Column[];
 }
-  
-export default function MainContent({ columns }: MainContentProps) { 
-  return ( 
-        <main className={styles.main}> 
-                <div className={styles.board}> 
-                        {columns.map(col => ( 
-                        <div key={col.id} className={styles.column}> 
-                        <h3 className={styles.colTitle}>{col.id} ({col.tasks.length})</h3> 
-                        {col.tasks.map((task, i) => ( 
-                        <div key={i} className={styles.card}>{task}</div> 
-            ))} 
-                </div> 
-            ))} 
-                </div> 
-        </main> 
-        ); 
-} 
+
+export default function MainContent({ columns }: MainContentProps) {
+  return (
+    <main className={styles.main}>
+      <div className={styles.board}>
+        {columns.map(col => (
+          <div key={col.id} className={styles.column}>
+            <h3 className={styles.columnTitle}>{col.title}</h3>
+            <div className={styles.taskList}>
+              {col.tasks.map((task, i) => (
+                <div key={i} className={styles.task}>{task}</div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
